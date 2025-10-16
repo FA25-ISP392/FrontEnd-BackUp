e5439663692dae3e50ef345b23f2c4519636c511
phat280405@gmail.com <phat280405@gmail.com>
2025-10-16 22:35:32 +0700

    Update UI/UX for Booking Table 16/10/2025

--- src/components/Manager/TableBookingsModal.jsx
+++ src/components/Manager/TableBookingsModal.jsx
@@ -273,22 +273,22 @@ export default function TableBookingsModal({
                     </svg>
                     <span>Số Người</span>
                   </div>
-                  <div className="flex items-center gap-2 text-left">
-                    <svg
-                      className="h-4 w-4 text-orange-500 flex-shrink-0"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                      />
-                    </svg>
-                    <span>Thời Gian</span>
-                  </div>
+                    <div className="flex items-center justify-center gap-2">
+                      <svg
+                        className="h-4 w-4 text-orange-500 flex-shrink-0"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                      <span>Thời Gian</span>
+                    </div>
                 </div>
               </div>
 
@@ -315,7 +315,7 @@ export default function TableBookingsModal({
                             {booking.seat} người
                           </span>
                         </div>
-                        <div className="text-neutral-600 text-sm font-medium">
+                        <div className="text-neutral-600 text-sm font-medium flex justify-center">
                           {formatHourMinute(booking.bookingDate)} -{" "}
                           {formatHourMinute(
                             addHoursToDate(booking.bookingDate, 2)
